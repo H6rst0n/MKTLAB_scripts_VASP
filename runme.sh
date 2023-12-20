cf17c74a99060248619db98493639f0b2cf5373f
H6rst0n <104990977+H6rst0n@users.noreply.github.com>
2023-12-20 15:46:26 +0800

    v1.1
    fix the error

--- runme.sh
+++ runme.sh
@@ -1,9 +1,9 @@
 mkdir -p $HOME/scripts
 cd ../
-mv ./scripts_for_vasp $HOME/scripts
-cd $HOME/scripts/scripts_for_vasp
+mv ./MKTLAB_scripts_VASP $HOME/scripts
+cd $HOME/scripts/MKTLAB_scripts_VASP
 chmod +x *py *sh
 chmod -x runme.sh
-echo 'export PATH=$HOME/scripts/scripts_for_vasp:$PATH' >> ~/.bashrc
+echo 'export PATH=$HOME/scripts/MKTLAB_scripts_VASP:$PATH' >> ~/.bashrc
 echo 'module load anaconda3/5.1.10' >> ~/.bashrc
 source ~/.bashrc
